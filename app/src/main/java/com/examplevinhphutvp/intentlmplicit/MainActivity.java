be26b3863a3bed90a882b79ce0580ee308d2b2fe
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-05-04 15:13:33 +0700

    camera

--- app/src/main/java/com/examplevinhphutvp/intentlmplicit/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/intentlmplicit/MainActivity.java
@@ -1,14 +1,65 @@
 package com.examplevinhphutvp.intentlmplicit;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Button mBtnCamera , mBtnGallery;
+    ImageView mImg;
+    int REQUEST_CODE_CAMERA = 1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnCamera = findViewById(R.id.buttonCamera);
+        mBtnGallery = findViewById(R.id.buttonGallery);
+        mImg = findViewById(R.id.imageview);
+
+        mBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        REQUEST_CODE_CAMERA
+                        );
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_CAMERA){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,REQUEST_CODE_CAMERA);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK &&  data != null) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            mImg.setImageBitmap(bitmap);
+        }
+            super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
